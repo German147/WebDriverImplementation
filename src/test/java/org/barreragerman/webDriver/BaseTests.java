ade6b45cfac9ac233c110dcffc75ea80c5618151
Germán <barreragerman27@gmail.com>
2022-07-07 09:33:29 -0300

    se busco un WebElement y se hace click sobre el mismo, tambien se cuentan los links que se muestran en la pagina

--- src/test/java/org/barreragerman/webDriver/BaseTests.java
+++ src/test/java/org/barreragerman/webDriver/BaseTests.java
@@ -1,18 +1,26 @@
 package org.barreragerman.webDriver;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
 
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver","src/resource/chromedriver.exe");
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/resource/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
+        driver.get("https://the-internet.herokuapp.com");
+        WebElement inputLinks = driver.findElement(By.linkText("Inputs"));
+        inputLinks.click();
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println("El numero total de links es " + links.size());
         System.out.println(driver.getTitle());
-        driver.quit();
+
     }
 
     public static void main(String[] args) {
